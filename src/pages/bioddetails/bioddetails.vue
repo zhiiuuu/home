7e0e5bb832a03dfe164bdb7fb854c7835851808d
SSypp <2435884205@qq.com>
2019-05-25 17:03:12 +0800

    标签

--- src/pages/bioddetails/bioddetails.vue
+++ src/pages/bioddetails/bioddetails.vue
@@ -3,18 +3,21 @@
         HeaderTab
         Details
         DetailsSlide
+        ThePag
         FooterTab
 </template>
 <script>
 import HeaderTab from './components/headertab.vue'
 import Details from './components/details.vue'
 import DetailsSlide from './components/detailsslide.vue'
+import ThePag from './components/thepag.vue'
 import FooterTab from '../components/footer_tab.vue'
 export default {
     components:{
         HeaderTab,
         Details,
         DetailsSlide,
+        ThePag,
         FooterTab
     },
     data() {
