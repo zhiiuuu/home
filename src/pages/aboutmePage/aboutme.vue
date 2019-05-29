87897072348dcb7285370f3a44d76f235e06254d
SSypp <2435884205@qq.com>
2019-05-29 15:07:46 +0800

    reset

--- src/pages/aboutmePage/aboutme.vue
+++ src/pages/aboutmePage/aboutme.vue
@@ -1,16 +1,13 @@
 <template lang='pug'>
     div
-        HeaderTab
         AboutmeBanner
         FooterTab
 </template>
 <script>
-import HeaderTab from './components/header_tab.vue'
 import AboutmeBanner from './components/aboutmeBanner.vue'
 import FooterTab from '../components/footer_tab.vue'
 export default {
   components:{
-      HeaderTab,
       AboutmeBanner,
       FooterTab
   },
